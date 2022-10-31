58c3137b483c32e622667ea80d5736c65ef58dc3
mufidaA <c1dkmo00@students.oamk.fi>
2022-10-31 23:24:10 +0200

    hidden key

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   useEffect(() => {
     const headers = {
       'Accept': 'application/json',
-      'Authorization': 'Bearer 9teFKbr4uHi4moGhA6-5'
+      'Authorization': 'Bearer '
     };
     const fetchData = async () => {
       try {
