555056517264db86a8a8b5d44d554ede82db81d5
mufidaA <c1dkmo00@students.oamk.fi>
2022-10-31 23:22:53 +0200

    init app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState()
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    const headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer 9teFKbr4uHi4moGhA6-5'
+    };
+    const fetchData = async () => {
+      try {
+          
+        const rawQuotes = await fetch('https://the-one-api.dev/v2/quote', {
+        headers: headers
+        });
+        const quotes = await rawQuotes.json();
+        const quote = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
+        setQuote(quote.dialog);
+        const rawCharacters = await fetch('https://the-one-api.dev/v2/character?_id=' + quote.character, 
+        { headers: headers });
+        const characters = await rawCharacters.json();
+        const character = characters.docs[0];
+        setCharacter(character.name);
+        
+      } catch (error) {
+        alert(error);
+        
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <blockquote>{quote}</blockquote>
+      <cite>- {character}</cite>
     </div>
+
   );
 }
 
